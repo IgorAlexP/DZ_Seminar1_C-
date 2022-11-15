90c4635abddc08b3bbd013fa7f9d47a99b2c2286
Igor Popkov <i.popkov@CPSURAL.LOCAL>
2022-11-15 16:25:38 +0500

    решил задачу 6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 6 Напишите программу, которая на вход принимает число и выдает, является ли число четным.*/
+
+Console.WriteLine("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("ЭТО ЧИСЛО ЯВЛЯЕТСЯ ЧЕТНЫМ");
+}
+else
+{
+   Console.WriteLine("ЭТО ЧИСЛО НЕ ЯВЛЯЕТСЯ ЧЕТНЫМ"); 
+}
